c55a3809cafbeea1c2aa59cd3825654807e204a1
Trần Đăng Dương <duongtran.bus@gmail.com>
2024-04-01 11:45:42 +0700

    67.

--- lib/questions.dart
+++ lib/questions.dart
@@ -10,6 +10,27 @@ class Questions extends StatefulWidget {
 class _QuestionsState extends State<Questions> {
   @override
   Widget build(context) {
-    return const Text('Questions Sreen');
+    return Column(
+      children: [
+       const Text('Questions Sreen'),
+       const Sizedbox(height: 30),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Answers 1'),
+          ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Answers 2'),
+          ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Answers 3'),
+          ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Answers 4'),
+          ),
+      ],
+    ) ;
   }
 }
